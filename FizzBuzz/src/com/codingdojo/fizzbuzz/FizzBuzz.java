c754cb1395dfc83bceabbc6e536df72a23cad555
Diego <dev.diegoduran@gmail.com>
2023-06-28 17:55:14 -0400

    first commit

---
+++ FizzBuzz/src/com/codingdojo/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,21 @@
+package com.codingdojo.fizzbuzz;
+
+public class FizzBuzz {
+
+	public String fizzBuzz(int numero) {
+
+		if (numero % 3 == 0 && numero % 5 == 0) {
+			return "FizzBuzz";
+		}
+
+		if (numero % 3 == 0) {
+			return "Fizz";
+		}
+		if (numero % 5 == 0) {
+			return "Buzz";
+		}
+
+		return "" + numero;
+	}
+
+}
---
+++ FizzBuzz/src/com/codingdojo/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+package com.codingdojo.fizzbuzz;
+
+public class FizzBuzzTest {
+
+	public static void main(String[] args) {
+
+		FizzBuzz x = new FizzBuzz();
+		System.out.println(x.fizzBuzz(10));
+		System.out.println(x.fizzBuzz(15));
+		System.out.println(x.fizzBuzz(2));
+		System.out.println(x.fizzBuzz(3));
+		System.out.println(x.fizzBuzz(5));
+		System.out.println(x.fizzBuzz(430));
+	}
+
+}
